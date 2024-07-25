e724deb0fcfc46c1c5dbcefe809b30e6cbfd93fc
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com.>
2024-07-25 17:45:59 -0500

    feat: :sparkles: the connection was done
    the js document is connected to mongo and mongo is conected to Railway

---
+++ main.js
@@ -0,0 +1,20 @@
+ import { connect } from "./helpers/connect.js";
+
+ console.log(process.env.MONGO_USER);
+// connect
+
+// async function main() {
+//     try {
+        
+//         const dbConnect = new connect();
+        
+//         await dbConnect.testConnection();
+        
+
+//     } catch (error) {
+//         console.error("Error durante la prueba de conexión:", error);
+//     }
+// }
+
+// main();
+
