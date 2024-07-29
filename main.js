876f69ce24568372f68864bf67ca7e7a79dc321b
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-07-29 10:15:08 -0500

    feat: :sparkles: Case of use 'Seats'
    feat: :construction: the function to update seats status is working but there are other specific requeriments to do
    
    [Hash]: ff53a0618e9d8d7ff1e7113332cd3990f0be9253
    
    feat: :construction: Workin in the second stage of the use cases
    
    [Hash]: 8a17b9bc1fb7125df6e2941de882fe39bea9a1f0
    
    feat: :construction: workin in the seat apis
    
    in the proces to finish up the requierments related to seats
    
    [Hash]: 5abf8641cde1e080b015bcf27d9b3c34982959e9
    
    feat: :memo: The document has being commented, and the methos were updated
    
    the seats stage is already done
    
    [Hash]: d84ef2c873a2cad4a2f44af43dae27657a193f68
    
    feat: :sparkles: the movie and seats cases are already finished
    
    both modules were commented including the main
    
    [Hash]: 98b53b26f6f07157b498338024d00d0ad8c1fb67

--- main.js
+++ main.js
@@ -1,25 +1,88 @@
 
-import { pelicula } from "./js/modules/pelicula.js";
+import { Pelicula } from "./js/modules/pelicula.js";
+import { Asiento } from "./js/modules/asiento.js";
 
-// connect
 
-// async function main() {
-//     try {
-        
-//         const dbConnect = new connect();
-        
-//         await dbConnect.testConnection();
-        
+// ! MOVIES CASES
 
-//     } catch (error) {
-//         console.error("Error durante la prueba de conexión:", error);
-//     }
-// }
 
-// main();
+//  let obj = new Pelicula() //todo: uncomment this before running each movies call
 
-let obj = new pelicula() 
+//! 1.1 Obtener informacion de todas la películas
 
 // console.table(await obj.getAllMoviesAndFunctionsInfo())
 
-console.table(await obj.getAnEspecificMovieInfo())
+// ! 1.2 Obtener informacion de una película en especifico
+
+ //console.table(await obj.getAnEspecificMovieInfo())
+
+
+
+
+
+
+// ! SEATS CASES
+
+// ! 2.2 Checks seat availability for a specific screening.
+/** 
+let obj = new Asiento() 
+
+const asientoInstance = new Asiento();
+const idFuncion = "64a7e409f7a42a24c8d7e826"; // todo: Replace with your actual id_funcion value
+asientoInstance.getAllSeatsByFunction(idFuncion).then(seats => {
+    console.log("Available seats:", seats);
+}).catch(err => {
+    console.error("Error:", err);
+});
+*/
+
+// ! 3.1 Reserve Seats: Allow the selection and reservation of seats for a specific screening.
+/** 
+const asientoInstance = new Asiento();
+const seatId = "64a7e409f7a42a24c8d7e82b"; //  todo: Replace with your actual seat id
+const functionId = "64a7e409f7a42a24c8d7e828"; //todo: Replace with your actual function id
+
+asientoInstance.setAReservetoASeatForAFunction(seatId, functionId).then(result => {
+    console.log(result.message);
+}).catch(err => {
+    console.error("Error:", err);
+});
+*/
+
+
+// ! Retrieves  reserved seat and its specific function, including the room and time. 
+/** 
+const asientoInstance = new Asiento();
+const seatId = "64a7e409f7a42a24c8d7e82b"; // todo: Replace with "seat_id" 
+
+asientoInstance.getAllTSeatsReservedByFunction(seatId).then(result => {
+    if (result.success) {
+        console.log("Seat details:", result.data);
+    } else {
+        console.log(result.message);
+    }
+}).catch(err => {
+    console.error("Error:", err);
+});
+*/
+
+/** 
+// !3.2 Cancel Seat Reservations: Allow the cancellation of a seat reservation already made.
+const asientoInstance = new Asiento();
+const seatId = "64a7e409f7a42a24c8d7e879"; //  todo: insert the SeatId to cancel a reserv
+
+asientoInstance.setACancelationtoAReservedSeat(seatId).then(result => {
+    console.log(result.message);
+}).catch(err => {
+    console.error("Error:", err);
+});
+*/
+
+
+
+
+
+//cliente
+//console.log(await obj.getAllClient)
+
+
