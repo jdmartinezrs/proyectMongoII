b6140aaacfb9b538f92463cc03e05c7b3b0cb362
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2024-07-30 11:20:30 -0500

    feat: :sparkles: Case of use 'Clients'
    feat: :construction: now it is posible to insert clients
    
    i have to I have to add that a role is assigned
    
    [Hash]: c08aaa4094004754436177d0cfd7c179f8828e8e
    
    feat: :sparkles: The method to show information to a user
    
    [Hash]: d8ff8a29504451dcdb797043d2c9cab953515abc
    
    feat: :sparkles: the method to insert a person and add a user is working
    
    the method to get the details of a person i also working
    
    [Hash]: cf391f5ddc5b9e05e7c7930840f4c4d749af6462
    
    feat: :sparkles: there are many cases that are already finished
    
    [Hash]: d05293e399db9926af931746386da5746adb79e5
    
    feat: :construction: implementing documentation and good practices
    
    the calls to the methods were deleted in the main
    
    [Hash]: 7280a8b3e5c506fe8891621dcf6421e543f88cb4
    
    feat: :sparkles: this is befor making the union with the main branch
    
    [Hash]: f80ae9b915d361069fb739639506aafce12ee29f

--- js/modules/asiento.js
+++ js/modules/asiento.js
@@ -7,8 +7,9 @@ ObjectId
 /**
  * * this is my Asiento class
  * @class
+ * Represents the Asiento class, which manages seat operations in the database.
  */
-export class Asiento extends connect{
+export class Asiento extends connect {
     static instance;
     collection
     constructor() {
@@ -18,31 +19,32 @@ export class Asiento extends connect{
         return this;
     }
 
-  
- /**
-     * 2.2 Checks seat availability for a specific screening.
-     * @param {ObjectId} id_funcion - The ObjectId of the function to check.
-     * @returns {Array} - List of available seats.
-     */
- async getAllSeatsByFunction(id_funcion) {
-  let res;
-  try {
-      await this.conexion.connect();
-      res = await this.collection.aggregate([
-          { $match: { 
-              id_funcion: new ObjectId(id_funcion), // Insert the "id_funcion" you want to see its available seats.
-              estado: "Por Asignar" // Matches seats with "Por Asignar" status, indicating availability.
-          } }, 
-          { $project: { _id: 0, fila: 1, codigo: 1, estado: 1, id_funcion: 1 } } 
-      ]).toArray(); 
-  } catch (err) {
-      console.error("Error al obtener los asientos:", err); // Logs any errors encountered.
-  } finally {
-      await this.conexion.close(); // Ensures the database connection is closed after the operation.
-  }   
-  return res;
-}
 
+    /**
+        * 2.2 Checks seat availability for a specific screening.
+        * @param {ObjectId} id_funcion - The ObjectId of the function to check.
+        * @returns {Array} - List of available seats.
+        */
+    async getAllSeatsByFunction(id_funcion) {
+        let res;
+        try {
+            await this.conexion.connect();
+            res = await this.collection.aggregate([
+                {
+                    $match: {
+                        id_funcion: new ObjectId(id_funcion),
+                        estado: "Por Asignar"
+                    }
+                },
+                { $project: { _id: 0, fila: 1, codigo: 1, estado: 1, id_funcion: 1 } }
+            ]).toArray();
+        } catch (err) {
+            console.error("Error al obtener los asientos:", err);
+        } finally {
+            await this.conexion.close();
+        }
+        return res;
+    }
 
 
 
@@ -53,154 +55,156 @@ export class Asiento extends connect{
      * @returns {Object} - Status of the update operation.
      */
     async setAReservetoASeatForAFunction(seatId, functionId) {
-      let res;
-      try {
-          await this.conexion.connect();
-          res = await this.collection.updateOne(
-              { _id: new ObjectId(seatId) }, // id del asiento
-              { 
-                  $set: { 
-                      estado: "Reservado",
-                      id_funcion: new ObjectId(functionId) // Añadir una función a un asiento específico
-                  }
-              }
-          );
-      } catch (err) {
-          console.error("Error al actualizar el asiento:", err);
-          return {
-              success: false,
-              message: "Error al actualizar el asiento"
-          };
-      } finally {
-          await this.conexion.close();
-      }
+        let res;
+        try {
+            await this.conexion.connect();
+            res = await this.collection.updateOne(
+                { _id: new ObjectId(seatId) },
+                {
+                    $set: {
+                        estado: "Reservado",
+                        id_funcion: new ObjectId(functionId)
+                    }
 
-      return {
-          success: res.acknowledged,
-          message: "Asiento actualizado con éxito" // ! if seat status is updated itis going to return this message
-      };
-  }
-
-  
-   /**
-     * Retrieves all seats for a specific function, including the room and time, 
-     * if the seat is "Reservado".
-     * @param {ObjectId} seatId - The ObjectId of the seat to check.
-     * @returns {Object} - Details of the seat or a message if not reserved.
-     */
-   async getAllTSeatsReservedByFunction(seatId) {
-    let res;
-    try {
-        // Open the database connection if not already open
-        await this.conexion.connect();
-
-        // Check the seat status
-        const seat = await this.collection.findOne({ _id: new ObjectId(seatId) });
-        
-        if (seat && seat.estado !== 'Reservado') {
+                }
+            );
+        } catch (err) {
+            console.error("Error al actualizar el asiento:", err);
             return {
                 success: false,
-                message: 'The seat is not reserved.' // ! if the seat is Not Reserved it is going to return this message
+                message: "Error al actualizar el asiento"
             };
+        } finally {
+            await this.conexion.close();
         }
 
-        // If the seat is reserved, get function details
-        res = await this.collection.aggregate([
-            {
-                $lookup: {
-                    from: 'funcion', // Name of the functions collection
-                    localField: 'id_funcion', // Field in the seats collection
-                    foreignField: '_id', // Field in the functions collection
-                    as: 'funcion' // Alias for the join result
-                }
-            },
-            {
-                $unwind: '$funcion' // Unwind the functions array to get individual documents
-            },
-            {
-                $match: {
-                    _id: new ObjectId(seatId) // Filter by seatId
-                }
-            },
-            {
-                $project: {
-                    _id: 0, // Exclude the _id field
-                    fecha_hora_inicio: '$funcion.fecha_hora_inicio', // Include function start time
-                    fecha_hora_fin: '$funcion.fecha_hora_fin', // Include function end time
-                    sala: '$funcion.sala' // Include the room information
-                }
-            }
-        ]).toArray();
-
-    } catch (err) {
-        console.error("Error al obtener los asientos:", err); // Log any errors encountered
         return {
-            success: false,
-            message: "Error al obtener los asientos"
+            success: res.acknowledged,
+            message: "Asiento actualizado con éxito"
         };
-    } finally {
-        // Close the database connection
-        await this.conexion.close();
-    }
-    
-    return {
-        success: true,
-        data: res
-    };
-}
-
-
-   /**
-     *3.2 Cancels a reservation for a specific seat if it is reserved.
-     * @param {ObjectId} seatId - The ObjectId of the seat to cancel the reservation.
-     * @returns {Object} - Status of the update operation.
-     */
-   async setACancelationtoAReservedSeat(seatId) {
-    let res;
-    try {
-        await this.conexion.connect();
-        
-        // Check the current status of the seat
-        const seat = await this.collection.findOne({ _id: new ObjectId(seatId) });
-        
-        if (!seat) {
-            return {
-                success: false,
-                message: "Seat not found."
-            };
-        }
-
-        if (seat.estado !== "Reservado") {
-            return {
-                success: false,
-                message: "The seat is not reserved and cannot be canceled."
-            };
-        }
-
-        // Proceed with cancellation
-        res = await this.collection.updateOne(
-            { _id: new ObjectId(seatId) }, // id del asiento
-            { 
-                $set: { 
-                    estado: "Por Asignar" // Update status to "Por Asignar"
-                }
-            }
-        );
-    } catch (err) {
-        console.error("Error al actualizar el asiento:", err);
-        return {
-            success: false,
-            message: "Error al actualizar el asiento"
-        };
-    } finally {
-        await this.conexion.close();
     }
 
-    return {
-        success: res.acknowledged,
-        message: "Reservation cancelled successfully" // Message after successful cancellation
-    };
-}
+
+    /**
+      * Retrieves all seats for a specific function, including the room and time, 
+      * if the seat is "Reservado".
+      * @param {ObjectId} seatId - The ObjectId of the seat to check.
+      * @returns {Object} - Details of the seat or a message if not reserved.
+      */
+    async getAllTSeatsReservedByFunction(seatId) {
+        let res;
+        try {
+
+            await this.conexion.connect();
+
+
+            const seat = await this.collection.findOne({ _id: new ObjectId(seatId) });
+
+            if (seat && seat.estado !== 'Reservado') {
+                return {
+                    success: false,
+                    message: 'The seat is not reserved.'
+                };
+            }
+
+
+            res = await this.collection.aggregate([
+                {
+                    $lookup: {
+                        from: 'funcion',
+                        localField: 'id_funcion',
+                        foreignField: '_id',
+                        as: 'funcion'
+                    }
+                },
+                {
+                    $unwind: '$funcion'
+                },
+                {
+                    $match: {
+                        _id: new ObjectId(seatId)
+                    }
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        fecha_hora_inicio: '$funcion.fecha_hora_inicio',
+                        fecha_hora_fin: '$funcion.fecha_hora_fin',
+                        sala: '$funcion.sala',
+                        estado: 1
+                    }
+                }
+            ]).toArray();
+
+        } catch (err) {
+            console.error("Error al obtener los asientos:", err);
+            return {
+                success: false,
+                message: "Error al obtener los asientos"
+            };
+        } finally {
+
+            await this.conexion.close();
+        }
+
+        return {
+            success: true,
+            data: res
+        };
+    }
+
+
+    /**
+      *3.2 Cancels a reservation for a specific seat if it is reserved.
+      * @param {ObjectId} seatId - The ObjectId of the seat to cancel the reservation.
+      * @returns {Object} - Status of the update operation.
+      */
+    async setACancelationtoAReservedSeat(seatId) {
+        let res;
+        try {
+            await this.conexion.connect();
+
+
+            const seat = await this.collection.findOne({ _id: new ObjectId(seatId) });
+
+            if (!seat) {
+                return {
+                    success: false,
+                    message: "Seat not found."
+                };
+            }
+
+            if (seat.estado !== "Reservado") {
+                return {
+                    success: false,
+                    message: "The seat is not reserved and cannot be canceled."
+                };
+            }
+
+
+            res = await this.collection.updateOne(
+                { _id: new ObjectId(seatId) },
+                {
+                    $set: {
+                        estado: "Por Asignar"
+                    }
+                }
+            );
+        } catch (err) {
+            console.error("Error al actualizar el asiento:", err);
+            return {
+                success: false,
+                message: "Error al actualizar el asiento"
+            };
+        } finally {
+            await this.conexion.close();
+        }
+
+        return {
+            success: res.acknowledged,
+            message: "Reservation cancelled successfully"
+        };
+    }
 
 
 }
--- js/modules/pelicula.js
+++ js/modules/pelicula.js
@@ -1,129 +1,112 @@
 import { connect } from "../../helpers/connect.js";
 
-/*Ejemplo*/
+
 /**
  * 1.1 Class representing a connection to the 'pelicula' collection in the database.
  * @extends connect
  */
 export class Pelicula extends connect {
-    static instance; // Holds the singleton instance of the Pelicula class
-    collection; // MongoDB collection for movies
+    static instance;
+    collection;
 
     /**
      * Creates an instance of the Pelicula class.
      * @constructor
      */
     constructor() {
-        super(); // Call the parent class constructor
-        this.collection = this.db.collection('pelicula'); // Initialize the 'pelicula' collection
-        Pelicula.instance = this; // Set the singleton instance
-        return this; // Return the instance of the class
+        super();
+        this.collection = this.db.collection('pelicula');
+        Pelicula.instance = this;
+        return this;
     }
 
 
-    
+    /**
+    * Retrieves information about all movies currently in theaters along with their scheduled showtimes.
+     * 
+     * @async
+     * @function
+     * @param {string} status - The status of the movies to retrieve (e.g., 'En cartelera').
+     * @returns {Promise<Object[]>} - A promise that resolves to an array of objects containing movie information and showtimes.
+ */
+    async getAllMoviesAndFunctionsInfo(status) {
+        let res = await this.collection.aggregate([
+            {
+                $match: { estado: status }
+            },
+            {
+                $lookup: {
+                    from: "funcion",
+                    localField: "_id",
+                    foreignField: "id_pelicula",
+                    as: "funciones"
+                }
+            },
+            {
+                $unwind: "$funciones"
+            },
+            {
+                $project: {
+                    _id: 0,
+                    titulo: 1,
+                    genero: 1,
+                    duracion: 1,
+                    estado: 1,
+                    sala: "$funciones.sala",
+                    "funciones.fecha_hora_inicio": 1,
+                    "funciones.fecha_hora_fin": 1
+                }
+            }
+        ]).toArray();
+
+        this.conexion.close();
+
+        return res;
+    }
+
+
 
     /**
- * Retrieves information about all movies currently in theaters along with their scheduled showtimes.
- * 
- * @async
- * @function
- * @returns {Promise<Object[]>} - A promise that resolves to an array of objects containing movie information and showtimes.
- */
-async getAllMoviesAndFunctionsInfo() {
-    let res = await this.collection.aggregate([
-        // Match documents in the movie collection with the status 'En cartelera' (In theaters)
-        {
-            $match: { estado: "En cartelera" } // ! it matches movies with "En cartelera" status , feel free to look for "Próximamente" or "Retirada de Cartelera" 
-        },
-        // Perform a lookup to join the movie collection with the showtimes collection
-        {
-            $lookup: {
-                from: "funcion", // The showtimes collection
-                localField: "_id", // Field in the movie collection
-                foreignField: "id_pelicula", // Field in the showtimes collection
-                as: "funciones" // Alias for the result of the join
+    * Retrieves specific information about a movie along with its scheduled showtimes.
+     * 
+     * @async
+     * @function
+     * @param {string} nombre - The title of the movie to retrieve information for.
+     * @returns {Promise<Object[]>} - A promise that resolves to an array of objects containing movie information and showtimes.
+     */
+    async getAnEspecificMovieInfo(nombre) {
+        let res = await this.collection.aggregate([
+            {
+                $match: { titulo: nombre } // Usa el parámetro titulo
+            },
+            {
+                $lookup: {
+                    from: "funcion",
+                    localField: "_id",
+                    foreignField: "id_pelicula",
+                    as: "funciones"
+                }
+            },
+            {
+                $unwind: "$funciones"
+            },
+            {
+                $project: {
+                    _id: 0,
+                    titulo: 1,
+                    genero: 1,
+                    duracion: 1,
+                    estado: 1,
+                    sala: "$funciones.sala",
+                    "funciones.fecha_hora_inicio": 1,
+                    "funciones.fecha_hora_fin": 1
+                }
             }
-        },
-        // Unwind the funciones array to output individual showtime documents
-        {
-            $unwind: "$funciones"
-        },
-        // Project the desired fields in the result
-        {
-            $project: {
-                _id: 0, // Exclude the _id field from the result
-                titulo: 1, // Include the movie title
-                genero: 1, // Include the movie genre
-                duracion: 1, // Include the movie duration
-                estado: 1, // Include the movie status
-                "sala": "$funciones.sala", // Include the showtime room
-                "funciones.fecha_hora_inicio": 1, // Include the showtime start date and time
-                "funciones.fecha_hora_fin": 1 // Include the showtime end date and time
-            }
-        }
-    ]).toArray();
-    
-    // Close the database connection
-    this.conexion.close();
-    
-    // Return the result with movie information and showtimes
-    return res;
-}
+        ]).toArray();
 
+        this.conexion.close();
 
+        return res;
+    }
 
- 
-
-
-   /**
- * 1.2 Retrieves specific information about a movie along with its scheduled showtimes.
- * 
- * @async
- * @function
- * @returns {Promise<Object[]>} - A promise that resolves to an array of objects containing movie information and showtimes.
- */
-async getAnEspecificMovieInfo() {
-    let res = await this.collection.aggregate([
-        // Match documents in the movie collection with the title 'El Gran Escape'
-        {
-            $match: { titulo: 'El Gran Escape' }// !here you inser the "movie title", to see the information about it 
-        },
-        // Perform a lookup to join the movie collection with the showtimes collection
-        {
-            $lookup: {
-                from: "funcion", // The showtimes collection
-                localField: "_id", // Field in the movie collection
-                foreignField: "id_pelicula", // Field in the showtimes collection
-                as: "funciones" // Alias for the result of the join
-            }
-        },
-        // Unwind the funciones array to output individual showtime documents
-        {
-            $unwind: "$funciones"
-        },
-        // Project the desired fields in the result
-        {
-            $project: {
-                _id: 0, // Exclude the _id field from the result
-                titulo: 1, // Include the movie title
-                genero: 1, // Include the movie genre
-                duracion: 1, // Include the movie duration
-                estado: 1, // Include the movie status
-                "sala": "$funciones.sala", // Include the showtime room
-                "funciones.fecha_hora_inicio": 1, // Include the showtime start date and time
-                "funciones.fecha_hora_fin": 1 // Include the showtime end date and time
-            }
-        }
-    ]).toArray();
-    
-    // Close the database connection
-    this.conexion.close();
-    
-    // Return the result with movie information and showtimes
-    return res;
-}
-
-    
-    
 }
